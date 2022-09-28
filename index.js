3fe663ffe553fcf87580a3b9c4dda00120de509d
Leo Volwahsen <volwahsenl@gmail.com>
2022-09-28 09:07:26 +0200

    g

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
-const setBg = () => {
-    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    document.body.style.backgroundColor = "#" + randomColor;
-    color.innerHTML = "#" + randomColor;
-};
+/* Event Listeners */
 
-genNew.addEventListener("click", setBg);
-setBg();
+//Syntax: element.addEventListener(event, function, useCapture);
+
+var myP = document.getElementById("myP");
+var myDiv = document.getElementById("myDiv");
+var myP2 = document.getElementById("myP2");
+var myDiv2 = document.getElementById("myDiv2");
+
+myP.addEventListener("click", function () {
+    myDiv.style.background = "lightblue";
+});
+
+// myP.onclick = function(){ myDiv.style.background = "lightblue" };
+
+myP.addEventListener("click", changeText, false);
+
+myDiv.addEventListener("click", changeText2, false);
+
+myDiv.addEventListener("mouseover", function () {
+    myDiv2.style.background = "#333";
+});
+
+function changeText() {
+    myP2.textContent = myP2.textContent +=
+        "According to a Cornell publication, the answer is ~700 pounds.";
+}
+
+function changeText2() {
+    myP2.textContent = myP2.textContent += "THE REAL ANSWER.";
+}
+
+myP.removeEventListener("click", changeText, false);
